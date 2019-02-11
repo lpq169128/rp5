53b132056a61376c23353972af80682f5638926a
lpq169128 <542245193@qq.com>
2019-02-11 21:21:31 +0800

    提交测试代码

---
+++ test-ssm/src/main/java/com/lpq/test.java
@@ -0,0 +1,21 @@
+package com.lpq;
+
+/**
+ * @ Author     ：LiPanQing
+ * @ Date       ：Created in 2019/2/11/011
+ * @ Description：${description}
+ * @ Modified By：
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("测试输出");
+        System.out.println("测试输出");
+        System.out.println("测试输出");
+        System.out.println("测试输出");
+        System.out.println("测试输出111");
+    }
+    public void test1(){
+        System.out.println("111111111");
+    }
+}
